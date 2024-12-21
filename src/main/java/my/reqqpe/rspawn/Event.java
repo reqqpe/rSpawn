e4fc7052795457f87632da08db751a40ad78ff19
egori <egorizipro2@gmail.com>
2024-12-21 14:51:24 +0300

    Добалено Тп при присоединение Тп при смерти

--- src/main/java/my/reqqpe/rspawn/Event.java
+++ src/main/java/my/reqqpe/rspawn/Event.java
@@ -6,13 +6,37 @@ import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+
+import java.util.Objects;
 
 public class Event implements Listener {
     private final Main plugin;
 
     public Event(Main plugin) {
         this.plugin = plugin;
+    }
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent e) {
+        Player player = e.getPlayer();
+        if (player.hasPermission(plugin.getConfig().getString("permission.tp-bypass", "rspawn.bypass"))) return;
+        if (plugin.getConfig().getBoolean("settings.tp-onDeath", false)) {
+            if (plugin.getGlobalSpawn() != null) {
+                e.setRespawnLocation(plugin.getGlobalSpawn());
+            }
+        }
+    }
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e ){
+        Player player = e.getPlayer();
+        if (player.hasPermission(plugin.getConfig().getString("permission.tp-bypass", "rspawn.bypass"))) return;
+        if (plugin.getConfig().getBoolean("settings.tp-onJoin", false)) {
+            if (plugin.getGlobalSpawn() != null) {
+                player.teleport(plugin.getGlobalSpawn());
+            }
+        }
     }
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
@@ -24,7 +48,7 @@ public class Event implements Listener {
         if (player.getGameMode() == GameMode.SPECTATOR) return;
         // Проверка: игрок застрял, если он внутри твёрдого блока,
         // и оба блока (текущий и над головой) твёрдые
-        if (plugin.getConfig().getBoolean("settings.tp-onBlock")) {
+        if (plugin.getConfig().getBoolean("settings.tp-onBlock", false)) {
             if (plugin.isStuck(location)) {
                 Location safeLocation = plugin.findSafeLocation(location);
                 if (safeLocation != null) {
@@ -42,7 +66,7 @@ public class Event implements Listener {
                     }
                     else {
                         if (plugin.getRegionSpawn(regionData) != null) {
-                            player.teleport(plugin.getRegionSpawn(regionData.toString()));
+                            player.teleport(Objects.requireNonNull(plugin.getRegionSpawn(regionData.toString())));
                         }
                         else {
                             player.teleport(safeLocation);
@@ -53,7 +77,7 @@ public class Event implements Listener {
         }
 
 
-        if (plugin.getConfig().getBoolean("settings.tp-fall")) {
+        if (plugin.getConfig().getBoolean("settings.tp-fall", false)) {
             if (location.getY() < plugin.getConfig().getInt("settings.tp-fall-y", -10)) {
                 if (plugin.getGlobalSpawn() != null) {
                     player.teleport(plugin.getGlobalSpawn());
